c70f4e81cd5429089e7b8d5c46d8e18db0915913
Max Leonard <max.leonard@kellogg.northwestern.edu>
2021-04-21 19:34:39 -0500

    HW3 complete

--- noober.js
+++ noober.js
@@ -12,6 +12,42 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
-  
+
+  // Store into memory the name, phone number, and starting and ending address
+  let passengerDetails = ride.passengerDetails
+  let pickup = ride.pickupLocation
+  let dropoff = ride.dropoffLocation
+
+  let passengerFirstName = passengerDetails.first 
+  let passengerLastName = passengerDetails.last 
+  let passengerNumber = passengerDetails.phoneNumber 
+
+  let startAdress = pickup.address
+  let startCity = pickup.city 
+  let startState = pickup.state 
+  let startZip = pickup.zip 
+
+  let endAdress = dropoff.address
+  let endCity = dropoff.city 
+  let endState = dropoff.state 
+  let endZip = dropoff.zip 
+
+  let rideType
+
+  // Decide which level of service to provide using conditionals
+  // Noober purple requests superscede other requests, so this goes first
+  if (ride.purpleRequested == true) {
+    rideType = 'Noober Purple'
+  } 
+ // if the number of passengers is over 3 we need an XL 
+  else if (ride.numberOfPassengers > 3) {
+    rideType = 'Noober XL'
+  } else {
+    // If the number of riders is 3 or below and no purple ride was requested it is a normal ride
+    rideType = 'Noober X'
+  }
+  console.log(`${rideType} Passenger: ${passengerFirstName} ${passengerLastName} - ${passengerNumber}. 
+  Pickup at ${startAdress}, ${startCity}, ${startState} ${startZip}. 
+  Drop-off at ${endAdress}, ${endCity}, ${endState} ${endZip}.`)
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
